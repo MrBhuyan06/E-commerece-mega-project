f406e26ac00a167a614e722b786f8fbbf10c67e3
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2022-12-09 18:13:50 +0530

    added user model part1

---
+++ backend/models/user.schema.js
@@ -0,0 +1,44 @@
+const { kMaxLength } = require('buffer')
+const mongoose=require('mongoose')
+const { object } = require('webidl-conversions')
+const AuthRole = require('../utils/authRole.js')
+
+const userSchema= mongoose.Schema(
+    {
+        name:
+        {
+            type:String,
+            required:[true, "Name is required"],
+            maxLength:[50, 'name must be less then 50']
+        },
+        email:
+        {
+            type:String,
+            required:[true, "email is required"],
+            unique:true
+            // regexr.com for regax
+        },
+        
+        password:
+        {
+            type:String,
+            required:[true, "password is required"],
+            minLength:[8, 'password  must be atleast 8 characters '],
+            select:false
+            
+        },
+        role:
+        {
+            type:String,
+            enum:Object.values(AuthRole),
+            default:AuthRole.USER
+        },
+        forgotPasswordToken: String,
+        forgotPasswordExpiry: Date
+        
+
+    },
+    {
+        timestamps: true
+    }
+)
