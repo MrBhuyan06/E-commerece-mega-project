d72e17ddcc326ef9f8ed3fc6a8e310e24ae092e0
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2022-12-15 12:49:07 +0530

    forget password function added in the modal

--- backend/models/user.schema.js
+++ backend/models/user.schema.js
@@ -5,6 +5,8 @@ const AuthRole = require('../utils/authRole.js')
 const bcrypt=require('bcryptjs')
 const JWT=require('jsonwebtoken')
 const crypto=require('crypto')
+// const { JWT_EXPIRY } = require('../config/index.js')
+const config = require('../config/index.js')
 
 const userSchema= mongoose.Schema(
     {
@@ -46,11 +48,57 @@ const userSchema= mongoose.Schema(
     }
 );
 
-// challenge 1- encrypt password
+// challenge 1- encrypt password -hooks
 // mongoose hooks
 
+userSchema.pre('save',  async function(next){
 
+    if(! this.isModified("password"))  return next();
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
 
+// mongoose schema method
+// add more feature directly to your schema
+
+userSchema.methods=
+{
+    //compare password
+    comparePassword: async function(enterPassword)
+    {
+        return await bcrypt.compare(enterPassword, this.password)
+    },
+    //genrate JWT token 
+    getJwtToken: function() 
+    {
+      return JWT.sign(
+        {
+            _id:this._id,
+            role:this.role
+        },
+        config.JWT_SECRET,
+        {
+            expiresIn:config.JWT_EXPIRY
+        }
+      )
+    },
+
+    generateForgotPasswordToken: function()
+    {
+         const forgotToken=crypto.randomBytes(20).toString('hex')
+
+         // step 1- save to DB
+         this.forgotPasswordToken= crypto
+         .createHash("sha256")
+         .update(forgotToken)
+         .digest('hex')
+         
+         this.forgotPasswordExpiry= Date.now() + 20 * 60 * 1000 // 20 min
+         // step 2 - return values to user
+
+         return forgotToken
+    }
+}
 
 
 
