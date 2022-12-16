1e13b52d5d8bbdbe4de013b6503f90a9b98c52f7
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2022-12-16 14:46:10 +0530

    AuthController- login and logout

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -46,4 +46,74 @@ exports.sigUp=asyncHandler(async (req,res) =>{
    user.password = undefined
 
    res.cookie("token", token, this.cookieOptions)
+   res.status(200).json({
+    success:true,
+    token,
+    user
+   })
+})
+
+
+/******************************************************
+ * @LOGIN
+ * @route http://localhost:5000/api/auth/signup
+ * @description User signIn Controller for login new user
+ * @parameters  email, password
+ * @returns User Object
+ ******************************************************/
+
+const asyncHandler = require("../services/asyncHandler.js");
+
+
+exports.login= asyncHandler(async (req, res) =>
+{
+    const { email, password}= req.body
+
+    if(  !email || !password)
+    {
+     throw new CustomError('Please fill all the fields', 400)
+    }
+
+    const user =   User.findOne({email}).select("+password")
+
+    if( !user)
+    {
+        throw new CustomError('Invalid credentials', 400)
+    }
+    const ispasswordMatch = await user.comparePassword(password)
+
+    if(ispasswordMatch)
+    {
+        const token= user.getJwtToken()
+        user.password = undefined
+        res.cookie('token', token, this.cookieOptions)
+        res.status(200).json({
+            success:true,
+            token,
+            user
+        })
+    }
+    throw new CustomError("Invalid credentials - pass", 400)
+})
+
+/******************************************************
+ * @LOGIN
+ * @route http://localhost:5000/api/auth/logout
+ * @description User Logout bby clearing user cookies
+ * @parameters  
+ * @returns Success message
+ ******************************************************/
+
+exports.logout= asyncHandler(async (_req, res) =>
+{
+
+    // res.clearCookie()
+    res.cookie("token", null,{
+        expires:new Date(Date.now()),
+        httpOnly:true
+    })
+    res.status(200).json({
+        success:true,
+        message: "Logged Out"
+    })
 })
