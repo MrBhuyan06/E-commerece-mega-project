76735db749668e51381533eab1ec2633549e9e43
Abhishek Bhuyan <abhisekhbhuyan0606@gmail.com>
2022-12-16 15:14:57 +0530

    added the mail helper

---
+++ backend/utils/mailHelper.js
@@ -0,0 +1,19 @@
+const transporter = require('../config/transporter.config.js')
+
+const config =require('../config/index.js')
+
+const mailHelper= async (options) =>
+{
+  const message = 
+  {
+    from: config.SMTP_MAIL_EMAIL, // sender address
+    to: options.email, // list of receivers
+    subject: options.subject,
+    text: options.text // plain text body
+    // html: "<b>Hello world?</b>", // html body
+  }
+
+  await transporter.sendMail(message)
+}
+
+module.exports=mailHelper
